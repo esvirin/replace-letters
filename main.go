adf7a45d9d5059314ae0febb4dc5d0a83fe9a045
Evgeny Svirin <esvirin@mail.com>
2024-05-14 19:02:00 +0200

    1.1

--- main.go
+++ main.go
@@ -10,32 +10,24 @@ import (
 func main() {
 	fmt.Println("Для выхода введите exit")
 	fmt.Println("Введите текст для замены символов:")
-
 	scanner := bufio.NewScanner(os.Stdin)
-
 	for scanner.Scan() {
-		
 		input := scanner.Text()
-
 		if input == "exit" {
 			break
 		}
-
 		fmt.Println("Результат: \n" + replace(input) + "\n")
 	}
 }
 
 func replace(input string) string {
-	cyrillic := map[rune]string{
+	replacement := map[rune]string{
 		'а': "a",
 		'о': "o",
 		'у': "y",
 		'е': "e",
 		'с': "c",
 		'т': "t",
-	}
-
-	latin := map[rune]string{
 		'a': "а",
 		'o': "о",
 		'y': "у",
@@ -47,19 +39,12 @@ func replace(input string) string {
 	result := strings.Builder{}
 
 	for _, letter := range input {
-
-		if rep, ok := cyrillic[letter]; ok {
-			result.WriteString(rep)
-			continue
-		}
-
-		if rep, ok := latin[letter]; ok {
+		if rep, ok := replacement[letter]; ok {
 			result.WriteString(rep)
 			continue
 		}
 
 		result.WriteRune(letter)
 	}
-
 	return result.String()
 }
